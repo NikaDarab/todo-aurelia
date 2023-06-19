f520274cbf345f43af034a1753ef4c96b569609d
Nikadarab <nikadarab@pm.me>
2023-06-19 14:37:34 -0500

    added add and remove methods to the ts file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+// export class App {
+//   public message = "Hello World!";
+// }
+
+import uniqid from "uniqid";
+
+interface ITodo {
+  id: number;
+  description: string;
+  done: boolean;
+}
+
 export class App {
-  public message = 'Hello World!';
+  heading: string;
+  todos: ITodo[];
+  todoDescription: string;
+
+  constructor() {
+    this.heading = "Todos";
+    this.todos = [];
+    this.todoDescription = "";
+  }
+
+  addTodo(): void {
+    if (this.todoDescription) {
+      this.todos.push({
+        id: uniqid(),
+        description: this.todoDescription,
+        done: false,
+      });
+      this.todoDescription = "";
+    }
+  }
+
+  removeTodo(todo: ITodo): void {
+    this.todos.filter((to) => to.id != todo.id);
+  }
 }
